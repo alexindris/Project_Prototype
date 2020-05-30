3eaa459cdfd095a6edca617e4ea945d8fb2293c6
MartaOG <59828554+MartaOG@users.noreply.github.com>
2020-05-30 17:07:45 +0200

    raspberry_

--- Project_Prototype_Client/programa.py
+++ Project_Prototype_Client/programa.py
@@ -3,7 +3,9 @@ import sys
 import Adafruit_DHT as dht
 import datetime
 import time
+import request
 
+url = 'https://192.168.1.133:8080/data'
 sensor = dht.DHT22
 id = "1"
 sensor_type = "Humidity"
@@ -13,24 +15,21 @@ while True:
     #humidity = dht.read_retry(sensor, pin)
     humidity, temperature = dht.read_retry(sensor, pin)
     humidity = round(humidity, 2)
+    
     date = datetime.datetime.now()
+    
     currentTime = date.strftime("%X")
     currentDay = date.strftime("%x")
+        
+    values = {"id": id,
+              "time": currentTime,
+              "date": currentDate,
+              "sensor": sensor,
+              "valor": humidity
+              }
     
-    results = [id, currentTime, currentDay, sensor, humidity]
-    title = ["ID", "Time: ", "Date: ", "Sensor: ", "Value: "]
-    rec = 0
-    
-    file = open("humidityRresults", "a")
-    
-    for r in results:
-        file.write("\n")
-        file.write(title[rec])
-        file.write(str(r))
-        rec = rec + 1
-
-    file.close()
+    x = requests.post(url, data = values)
     time.sleep(1)
 
-#print('Temp={0:0.1f} *c, Hum={1:0:01f} %'.format(temperature, humidity))
 sys.exit(0)
+
