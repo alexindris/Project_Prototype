a08ae08b9a55ea55d5522d6bfbe11d5bf493c85e
Alex Tello <atello@edu.tecnocampus.cat>
2020-05-31 20:09:38 +0200

    python change

--- Project_Prototype_Client/programa.py
+++ Project_Prototype_Client/programa.py
@@ -5,29 +5,30 @@ import datetime
 import time
 import requests
 
-url = 'http://192.168.0.165:8080/data'
-sensor = dht.DHT22
-id = "1"
-sensor_type = "Humidity"
-pin = 4
-humidity = 0
-while True:
-    #humidity = dht.read_retry(sensor, pin)
-    #humidity, temperature = dht.read_retry(sensor, pin)
-    date = datetime.datetime.now()
-    
-    currentTime = date.strftime("%X")
-    currentDay = date.strftime("%x")
-        
-    values = {"id": id,
-              "time": currentTime,
-              "date": currentDay,
-              "sensor": sensor_type,
-              "value": humidity
-              }
-    x = requests.post(url, json = values)
-    print(x.text)
-    time.sleep(1)
+def loop(url):
+    sensor = dht.DHT22
+    id = "1"
+    sensor_type = "Humidity"
+    pin = 4
+    humidity = 0
+    while True:
+        humidity, temperature = dht.read_retry(sensor, pin)
+        date = datetime.datetime.now()
 
-sys.exit(0)
+        currentTime = date.strftime("%X")
+        currentDay = date.strftime("%x")
+
+        values = {"id": id,
+                  "time": currentTime,
+                  "date": currentDay,
+                  "sensor": sensor_type,
+                  "value": humidity
+                  }
+        x = requests.post(url, json = values)
+        print(x.text)
+        time.sleep(1)
+
+url = 'http://'+ input("Introduce the website of the api ex(www.yourapi.com)")+':8080/data'
+loop(url)
+
 
