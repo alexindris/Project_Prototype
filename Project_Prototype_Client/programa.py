9130fb4a327521303a53d3c96fe8b5af08645d32
Alex Tello <atello@edu.tecnocampus.cat>
2020-05-31 21:25:16 +0200

    Prototype end

--- Project_Prototype_Client/programa.py
+++ Project_Prototype_Client/programa.py
@@ -5,35 +5,35 @@ import datetime
 import time
 import requests
 
+url = 'https://tcm-prototype-apirest.herokuapp.com/data'
+sensor = dht.DHT22
+id = "1"
+sensor_type = "Humidity"
+pin = 4
+humidity = 0
 
-def loop(url):
-    sensor = dht.DHT22
-    id = "1"
-    sensor_type = "Humidity"
-    pin = 4
-    humidity = 0
-    while True:
-        humidity, temperature = dht.read_retry(sensor, pin)
-        date = datetime.datetime.now()
+while True:
+    
+    humidity, temperature = dht.read_retry(sensor, pin)
+    date = datetime.datetime.now()
 
-        currentTime = date.strftime("%X")
-        currentDay = date.strftime("%x")
+    currentTime = date.strftime("%X")
+    currentDay = date.strftime("%x")
 
-        values = {"id": id,
-                  "time": currentTime,
-                  "date": currentDay,
-                  "sensor": sensor_type,
-                  "value": humidity
-                  }
-        try:
-            x = requests.post(url, json=values)
-        except requests.exceptions.RequestException as e:  # This is the correct syntax
-            print("ConnectionError:"+str(e))
-            exit(0)
+    values = {"id": id,
+              "time": currentTime,
+              "date": currentDay,
+              "sensor": sensor_type,
+              "value": humidity
+              }
+    try:
+        x = requests.post(url, json=values)
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        print("ConnectionError:"+str(e))
+        exit(0)
 
-        print(x.text)
-        time.sleep(1)
+    print(x.text)
+    time.sleep(1)
 
 
-url = 'http://' + str(raw_input("Introduce the website of the api ex(www.yourapi.com): ")) + ':8080/data'
-loop(url)
+
