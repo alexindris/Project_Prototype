5655d1195c286a6ca16df4e2f78ddd0d3ad369c8
Alex Tello <atello@edu.tecnocampus.cat>
2020-06-01 13:22:34 +0200

    CORS ADD

--- Project_Prototype_ApiREST/src/main/java/com/tcm/prototype/api/DataRestController.java
+++ Project_Prototype_ApiREST/src/main/java/com/tcm/prototype/api/DataRestController.java
@@ -3,6 +3,7 @@ package com.tcm.prototype.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +35,7 @@ public class DataRestController {
 		return gson.toJson(data);
 		
 	}
-	
+	@CrossOrigin(origins = "https://alexindris.github.io/Project_Prototype/")
 	@GetMapping("/data")
 	public String getAllData() throws InvalidParamException, NotFoundException {
 		List < DataDTO> alldata = dataController.getAllData();
